9e7fad76f38b21456f4629b2d8cd1994d93d40d5
arodriguezfebres <arodriguezfebres@gmail.com>
2020-03-17 22:38:23 -0500

    Removing bash-functionality from pwsh script.

--- infra/cloudformation/scripts/0_build/install-poetry.ps1
+++ infra/cloudformation/scripts/0_build/install-poetry.ps1
@@ -1,3 +1,1 @@
 Invoke-Expression -Command "(Invoke-WebRequest -Uri https://raw.githubusercontent.com/python-poetry/poetry/master/get-poetry.py -UseBasicParsing).Content | python"
-Invoke-Expression -Command "source $HOME/.poetry/env"
-Invoke-Expression -Command "poetry install"
