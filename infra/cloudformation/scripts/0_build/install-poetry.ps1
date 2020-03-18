9e5bae76b65e53ca460e6a5e21c2c2ff02d80cd3
arodriguezfebres <arodriguezfebres@gmail.com>
2020-03-17 22:28:08 -0500

    Poetry invocation fix.

--- infra/cloudformation/scripts/0_build/install-poetry.ps1
+++ infra/cloudformation/scripts/0_build/install-poetry.ps1
@@ -1,2 +1,2 @@
-Invoke-Expression -Command "Invoke-WebRequest -Uri (https://raw.githubusercontent.com/python-poetry/poetry/master/get-poetry.py -UseBasicParsing).Content | python"
+Invoke-Expression -Command "(Invoke-WebRequest -Uri https://raw.githubusercontent.com/python-poetry/poetry/master/get-poetry.py -UseBasicParsing).Content | python"
 Invoke-Expression -Command "poetry install"
