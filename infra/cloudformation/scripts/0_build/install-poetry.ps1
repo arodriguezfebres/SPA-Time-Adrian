52c609bbf851040045d581a07f7494584b655b75
arodriguezfebres <arodriguezfebres@gmail.com>
2020-03-17 22:32:10 -0500

    Update current shell to include poetry in path.

--- infra/cloudformation/scripts/0_build/install-poetry.ps1
+++ infra/cloudformation/scripts/0_build/install-poetry.ps1
@@ -1,2 +1,3 @@
 Invoke-Expression -Command "(Invoke-WebRequest -Uri https://raw.githubusercontent.com/python-poetry/poetry/master/get-poetry.py -UseBasicParsing).Content | python"
+Invoke-Expression -Command "source $HOME/.poetry/env"
 Invoke-Expression -Command "poetry install"
