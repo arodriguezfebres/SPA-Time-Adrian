f0d85fd9bf724375294d6279f2ff51dbe4ae8b7a
arodriguezfebres <arodriguezfebres@gmail.com>
2020-03-18 00:55:22 -0500

    Install get-pip for python3.

--- infra/cloudformation/scripts/0_build/install-python.sh
+++ infra/cloudformation/scripts/0_build/install-python.sh
@@ -4,3 +4,5 @@ sudo add-apt-repository -y ppa:deadsnakes/ppa
 sudo apt update
 sudo apt purge python2.7-minimal
 sudo apt install -y python3.7
+wget https://bootstrap.pypa.io/get-pip.py
+sudo python3 get-pip.py
