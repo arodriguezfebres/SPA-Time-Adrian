62c567ae069c3af4ee499fb519f940a197793ce8
arodriguezfebres <arodriguezfebres@gmail.com>
2020-03-17 23:58:32 -0500

    Attempting to stop 2.7 dependency conflicts.

--- infra/cloudformation/scripts/0_build/install-python.sh
+++ infra/cloudformation/scripts/0_build/install-python.sh
@@ -2,4 +2,5 @@ sudo apt update
 sudo apt install -y software-properties-common
 sudo add-apt-repository -y ppa:deadsnakes/ppa
 sudo apt update
+sudo apt purge python2.7-minimal
 sudo apt install -y python3.7
